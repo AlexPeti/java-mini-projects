41df177992fc365033d9bdd2cec869b7db225439
AlexPeti <alexandros.peteinaris@hotmail.com>
2022-12-20 23:37:08 +0200

    CombinationsSix v0.4

--- CombinationsSixApp.java
+++ CombinationsSixApp.java
@@ -161,8 +161,10 @@ public class CombinationsSixApp {
         int count = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] % 10 == arr[i++] % 10) {
-                count++;
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[i] != arr[j] && arr[i] % 10 == arr[j] % 10) {
+                    count++;
+                }
             }
         }
         return count <= ending;
